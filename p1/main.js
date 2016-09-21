9aa94908217d9b4f37509f1194a7caaa75ce7424
coke <coke@mta.cl>
2016-09-21 19:23:57 -0300

    init p1

---
+++ p1/main.js
@@ -0,0 +1,12 @@
+/* 
+	ejercicio	: 1
+	author 		: coke
+
+	 ¿Cuál es el problema con el siguiente código?
+		var	alumno1	=	{nombre:	"Matías",	edad:5}
+		var	alumno2	=	alumno1
+		alumno2.nombre	=	"Sebastián
+
+
+	R: Al asignar el valor de Sebastián al objecto alumno2, se cambiará también el nombre de alumno1.
+*/
