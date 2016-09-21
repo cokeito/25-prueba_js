27b9600f6b741b974015b4c546b48231474df7d8
coke <coke@mta.cl>
2016-09-21 20:38:14 -0300

    init p3 , function constructora done, con validaciones

---
+++ p3/main.js
@@ -0,0 +1,65 @@
+/* 
+	ejercicio	: 3
+	author 		: coke
+
+	Pregunta 3
+
+	Se pide modelar el siguiente problema:
+	
+	En una sala de clases hay alumnos, cada alumno tiene notas y un promedio final. Se pide:
+	
+	Crear la función constructora alumno que recibe el nombre de éste y un arreglo con las notas
+	
+	La función constructora debe revisar que el nombre sea un string y que el arreglo de notas sean integers
+	
+	Crear el método promedio que devuelva el promedio de notas de todos los alumnos.
+	
+	alumno1	=	Student.new("Oscar", [10, 9, 10])
+	alumno1.promedio() //	9.6666667
+
+
+*/
+	
+		function Alumnos(name, notes) {
+
+			if (typeof name == 'string' ) {
+				//validation of name ok
+					if ( Array.isArray( notes )) {
+						var error = false;
+
+						for (var i = 0; i < notes.length; i++) {
+							console.log(notes[i]);
+
+							if ( notes[i] % 1 === 0) {
+								error = 0;
+							} else {
+								error = 1;
+								console.log('la nota '+notes[i]+' no es del tipo int');
+								return false;
+							}
+						}
+
+						console.log('e: '+error);
+						if ( error === 0) {
+							console.log('creando obj');
+							this.name 	= name;
+							this.notes	= notes;
+						}
+
+					} else {
+						console.log('No se puede crear el objeto, ya que las notas no son partes de un array');
+					}
+
+			} else {
+				console.log('No se puede crear el ojbecto, ya que el nombre no es un string');
+	
+			}
+		}
+
+
+	/* 	forma de uso 
+		===========/
+		
+		var copied = copyObject(test); 
+
+	*/
