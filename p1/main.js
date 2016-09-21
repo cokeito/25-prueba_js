a19730987c0ca850b2a781dc52cc6fce7952f2b2
coke <coke@mta.cl>
2016-09-21 19:32:05 -0300

    init p2

--- p1/main.js
+++ p1/main.js
@@ -5,8 +5,8 @@
 	 ¿Cuál es el problema con el siguiente código?
 		var	alumno1	=	{nombre:	"Matías",	edad:5}
 		var	alumno2	=	alumno1
-		alumno2.nombre	=	"Sebastián
+		alumno2.nombre	=	"Sebastián"
 
 
-	R: Al asignar el valor de Sebastián al objecto alumno2, se cambiará también el nombre de alumno1.
+	R: Al asignar el valor de Sebastián al objecto alumno2, se cambiará también el nombre de alumno1, ya que alumno1 es una referencia de alumno2, por ende cualquier propiedad que se cambie afectará a ambos.
 */
---
+++ p2/main.js
@@ -0,0 +1,18 @@
+/* 
+	ejercicio	: 2
+	author 		: coke
+
+	Pregunta 2
+	Crear una función que devuelva una copia de un objeto, si el objeto contiene un arreglo éste también debe ser una copia.
+
+*/
+
+
+	function copyObject(obj) {
+
+		for (var i = 0; i < obj.length; i++) {
+			
+
+		}
+		
+	}
