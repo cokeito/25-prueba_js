2164dc264a91b782e1a8b02124fa7e1502d5af3e
coke <coke@mta.cl>
2016-09-21 20:08:16 -0300

    p2 terminada

--- p2/main.js
+++ p2/main.js
@@ -6,13 +6,24 @@
 	Crear una función que devuelva una copia de un objeto, si el objeto contiene un arreglo éste también debe ser una copia.
 
 */
-
+	
+	var test = {'name':'coke', 'age':35, 'notes': [7,6,7] };
 
 	function copyObject(obj) {
 
-		for (var i = 0; i < obj.length; i++) {
-			
+		var newObj = {};
 
+		//recorro el objeto 
+		for (var key in obj) {
+			newObj[key] = obj[key];
 		}
 		
+		return newObj;
 	}
+
+	/* 	forma de uso 
+		===========/
+		
+		var copied = copyObject(test); 
+
+	*/
